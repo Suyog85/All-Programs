8219bad95e4d7daf2997b17efda381e36b2a320d
Suyog Jagdale <163533573+Suyog85@users.noreply.github.com>
2024-07-28 21:20:21 +0530

    Add files via upload

---
+++ Programs/Web Development/JavaScript/JavaScriptBasic/basic.js
@@ -0,0 +1,26 @@
+console.log('My name is Suyog');
+console.log(2+3);
+console.warn('This is an Warning');
+console.error('This is an error');
+
+
+// Variables
+
+var a=20;
+let b= 30;
+const c =40;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Display name from the variable on the page
+const myName = 'Suyog';
+document.querySelector('#name-display').innerText=myName;
+
+// eval method - used to convert string numbers into numbers
+
+console.log(eval('2+3+4+5'));
+
+
+
---
+++ Programs/Web Development/JavaScript/JavaScriptBasic/index.js
@@ -0,0 +1,5 @@
+console.log('Hello Suyog');
+let a=10;
+a=15;
+console.log(a);
+// alert('Welcome to alert')
